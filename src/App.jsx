f84a545d4539aec9cf01741e00e3a0d9bd40178a
binayakbhandari <binayakbhandari60@gmail.com>
2025-05-05 23:33:11 +0545

    Completed Day 19 and 20

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import UseState from './components/UseState'
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={ <Home /> } />
+      <Route path='/usestate' element={ <UseState /> } />
     </Routes>
     </BrowserRouter>
   )
